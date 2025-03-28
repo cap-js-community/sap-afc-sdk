819f247e5a421bb072feedf7923ba9ede67f2181
Oliver Klemenz <oliver.klemenz@sap.com>
2025-03-28 12:16:28 +0100

    Kyma CI

--- scripts/build-kyma.sh
+++ scripts/build-kyma.sh
@@ -5,7 +5,8 @@ export SERVER_URL=https://afcsdk-srv-afc-sdk-dev.c-589f01f.stage.kyma.ondemand.c
 export BROKER_PASSWORD_HASH="sha256:pg78nhUJXTcWcD2NCOlT6hkQQA8qwxLgsZiQasutisc=:1eBRSsW3Sd7Hm3bukWS399zGNJb7d9IdnDe1uDTkSNU="
 export BROKER_SERVICE_ID="d48d9d0b-4320-439e-8f4e-2ab0294bd971"
 export BROKER_SERVICE_PLAN_ID="b7609272-ce22-41e0-a605-1ad3318ef2d5"
-export CONTAINER_REPOSITORY=docker.io/max8995
+export CONTAINER_REPOSITORY=afcsdk.common.repositories.cloud.sap
+# kubectl get gateway -n kyma-system kyma-gateway -o jsonpath='{.spec.servers[0].hosts[0]}' | grep -v '^$'
 export GLOBAL_DOMAIN=c-589f01f.stage.kyma.ondemand.com
 
 rm -rf temp/afcsdk
@@ -17,5 +18,3 @@ npm install github:cap-js-community/sap-afc-sdk
 afc init kyma
 afc add -a broker,stub,mock,sample,test,http
 npm test
-docker system prune -a -f
-ctz containerize.yaml --push --logs
