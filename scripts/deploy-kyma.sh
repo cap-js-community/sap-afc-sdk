b3b0ef8db3005109311ab9eb9e0f121e59156a7d
Oliver Klemenz <oliver.klemenz@sap.com>
2025-03-31 17:24:15 +0200

    Kyma CI

--- scripts/deploy-kyma.sh
+++ scripts/deploy-kyma.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd temp/afcsdk
-#docker system prune -a -f
+docker system prune -a -f
 ctz containerize.yaml --push --logs
 helm upgrade --install afcsdk ./gen/chart -n afc-sdk-dev
 kubectl rollout restart deployment -n afc-sdk-dev
