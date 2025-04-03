06041316db8943b5eae10c4a02f20061737f34f9
Oliver Klemenz <oliver.klemenz@sap.com>
2025-04-03 08:55:36 +0200

    bin test

---
+++ bin/jest.config.js
@@ -0,0 +1,29 @@
+"use strict";
+
+module.exports = {
+  reporters: ["default"],
+  automock: false,
+  bail: false,
+  clearMocks: false,
+  collectCoverage: false,
+  collectCoverageFrom: [],
+  coverageDirectory: "reports/coverage/unit/",
+  coverageReporters: ["lcov", "text"],
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
+  moduleDirectories: ["node_modules"],
+  modulePathIgnorePatterns: [],
+  resetMocks: false,
+  resetModules: false,
+  testMatch: ["**/test/**/*.test.js"],
+  testPathIgnorePatterns: ["/templates/"],
+  verbose: true,
+  maxWorkers: 2,
+  testTimeout: 60000,
+};
---
+++ bin/test/build.test.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+const shelljs = require("shelljs");
+
+const workingDir = path.join(__dirname, "..", "temp");
+const projectDir = path.join(workingDir, "afcsdk");
+
+process.env.APPROUTER_URL = "https://afcsdk-approuter.cloud.sap";
+process.env.SERVER_URL = "https://afcsdk-srv.cloud.sap";
+process.env.BROKER_PASSWORD_HASH = "sha256:xxx=:xxx=";
+process.env.BROKER_SERVICE_ID = "e25a901f-43d1-43c0-bbd6-48b9c6aecb61";
+process.env.BROKER_SERVICE_PLAN_ID = "958cbd91-af93-4fdd-a793-16c82ae9e70b";
+process.env.CONTAINER_REPOSITORY = "afcsdk.common.repositories.cloud.sap";
+process.env.GLOBAL_DOMAIN = "xxx.stage.kyma.ondemand.com";
+
+const Commands = {
+  BEFORE: [
+    "cd temp",
+    "npx cds init afcsdk",
+    "cd afcsdk",
+    "npm install ../../../",
+    "afc init cf",
+    "afc add -a broker,stub,mock,sample,test,http",
+  ],
+  CF: ["afc init cf"],
+  KYMA: ["afc init kyma"],
+  AFTER: ["afc add -a broker,stub,mock,sample,test,http"],
+};
+
+const Files = {
+  ALL: ["package.json", "app/router/xs-app.json", "srv/broker.json", "srv/catalog.json"],
+  CF: ["mta.yaml"],
+  KYMA: ["chart/values.yaml", "containerize.yaml"],
+};
+
+describe("Build", () => {
+  beforeEach(() => {
+    if (fs.existsSync(workingDir)) {
+      fs.rmdirSync(workingDir, { recursive: true });
+    }
+    fs.mkdirSync(workingDir);
+  });
+
+  afterAll(() => {
+    if (fs.existsSync(workingDir)) {
+      fs.rmdirSync(workingDir, { recursive: true });
+    }
+  });
+
+  it("CF", async () => {
+    shelljs.exec([...Commands.BEFORE, ...Commands.CF, ...Commands.AFTER].join(" && "));
+    for (const file of [...Files.ALL, ...Files.CF]) {
+      const content = fs.readFileSync(path.join(projectDir, file), "utf8");
+      expect(content).toMatchSnapshot();
+    }
+  });
+
+  it("Kyma", async () => {
+    shelljs.exec([...Commands.BEFORE, ...Commands.KYMA, ...Commands.AFTER].join(" && "));
+    for (const file of [...Files.ALL, ...Files.KYMA]) {
+      const content = fs.readFileSync(path.join(projectDir, file), "utf8");
+      expect(content).toMatchSnapshot();
+    }
+  });
+});
